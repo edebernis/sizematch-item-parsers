749787c2f0126118c6f2c19bbd6c08074197ec7a
Emeric de Bernis <emeric.debernis@wifirst.fr>
2020-03-24 22:54:07 +0100

    add options on puppeteer scraping

--- src/parsers/ikea.js
+++ src/parsers/ikea.js
@@ -11,13 +11,15 @@ class IKEAParser extends Parser {
     async parse(obj) {
         const page = await this.browser.newPage();
         await page.goto(obj.urls[0], {
-            timeout: this.config.fetchPageTimeout
+            timeout: this.config.fetchPageTimeout,
+            waitUntil: 'networkidle2'
         });
 
         const metadata = await page.evaluate('utag_data');
         const dimensions = await this.DLtoObj(page, '#pip_dimensions');
         const id = metadata.product_ids[0];
 
+        await page.close();
         return new Item(id, SOURCE, obj.lang, dimensions, metadata);
     }
 }
